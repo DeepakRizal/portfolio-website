c31da9d20ec0e4c81129b55a2a26d9791f6feb81
Deepak Rizal <deepak2000rizal@gmail.com>
2025-03-22 11:56:24 +0530

    Initial commit

---
+++ src/components/navbar/NavbarMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarLogo from "./NavbarLogo";
+
+const NavbarMain = () => {
+  return (
+    <nav>
+      <NavbarLogo />
+    </nav>
+  );
+};
+
+export default NavbarMain;
